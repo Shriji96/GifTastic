df8d1892d2597e2021bf7514633a7fbb204e6ee5
Shriji96 <shriji_96@yahoo.com>
2019-03-30 16:51:58 -0700

    have questions

--- index.js
+++ index.js
@@ -1,17 +1,50 @@
-var players = ["Kobe Bryant", "Michael Jordan", "Lebron James", "Stephen Curry", "Kevin Durant", "Magic Johnson", "Roger Federer", "Novak Djokovic", "Rafael Nadal", "Sachin Tendulkar"];
-
 $(document).ready(function() {
-    for (var i = 0; i < players.length; i++) {
-        $("#players").append("<button type='button' onclick='searchGif(\"" + players[i] + "\")' class='btn btn-primary' value=' " + players[i] + "'> " + players[i] + " </button>");
-    }
-});
-
-function click() {
-    var userInput = $('#player-input').val();
-    searchGif(userInput);
-}
-
-function submitButtonClicked() {
-    var userInput = $('#player-input').val();
-
     
+    var athletes = ["Kobe Bryant", "Michael Jordan", "Lebron James", "Stephen Curry", "Kevin Durant", "Magic Johnson", "Roger Federer", "Novak Djokovic", "Rafael Nadal", "Sachin Tendulkar"];
+
+
+    function renderButtons() {
+        $('#buttons-view').empty();
+        
+        for(var i = 0; i < athletes.length; i++) {
+        var button = $('<button>');
+            button.addClass('athlete');
+            button.attr('data-name', athletes[i]);
+            button.text(athletes[i]);
+            $('#athletes-buttons').append(button);
+        }
+       }
+
+        $(document).on('click', '.athletes-button', function() {
+       
+
+    var athlete = $(this).attr('data-name');
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + athlete + "&api_key=GphJwpkIXq4l6EGqvH1Tg4nztLgjIM5E&q=&limit=10&offset=0&lang=en";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (athleteData) {
+      var results = athleteData.data;
+      console.log(results)
+
+      for (var i = 0; i < results.length; i++) {
+        var athleteDiv = $('<div>').addClass(athlete-gifs);
+        var rating = results[i].rating;
+        var p = $('<p>').text('Rating: ' + rating);
+        
+      }
+
+    });
+ });
+$('#add-athlete').on('click', function(event) {
+ event.preventDefault();
+var athleteInput = $('athlete-input').val().trim();
+button.push(athlete);
+
+renderButtons();
+}); 
+renderButtons();
+
+});
+   
